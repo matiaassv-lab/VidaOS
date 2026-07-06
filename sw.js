aff15168b8ce3a5e4f380a14a074a78241ca6041
matiaassv-lab <matiaas.sv@gmail.com>
2026-07-06 15:57:17 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vidaos-v6';
+const CACHE='vidaos-v7';
 const FILES=['./','./index.html','./manifest.json','./icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
